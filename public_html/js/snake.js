7a4aeb49d9abbac52c46a019cb5123028dab788c
Adrian Esquivel <Adrianesquivel1112@yahoo.com>
2014-10-06 12:13:01 -0700

    snake.js
    worked on the snake.js page and learned about DOMS

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -5,5 +5,11 @@ var screenWidth;
 var screenHeight;
 
 function gameInitialize() {
+    var canvas = document.getElementById("game-screen");   
+}
+function gameLoop() {
+    
+}
+function gameDraw() {
     
 }
