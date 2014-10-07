2c96fec66b9cdf774272110ab83fd5c10758ffd5
Adrian Esquivel <Adrianesquivel1112@yahoo.com>
2014-10-07 11:26:33 -0700

    height, width
    made height and width of page on snake.js

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -5,11 +5,20 @@ var screenWidth;
 var screenHeight;
 
 function gameInitialize() {
-    var canvas = document.getElementById("game-screen");   
+    var canvas = document.getElementById("game-screen");
+    context = canvas.getContext("2D");
+    
+    screenWidth = window.innerWidth;
+    screenHeight = window.innerHeight;
+    
+    canvas.width = screenWidth;
+    canvas.height = screenHeight;
+    
 }
 function gameLoop() {
     
 }
 function gameDraw() {
-    
+    context.fillStyle = "rgb(180, 250, 213)";
+    context.fillRect(0, 0, screenWidth, screenHeight);
 }
