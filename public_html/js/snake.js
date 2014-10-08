819fac81b80d774376a15f0c0f22d3eef67b40c4
Adrian Esquivel <Adrianesquivel1112@yahoo.com>
2014-10-08 11:27:49 -0700

    making the page
    put color on my page and adding variables

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -1,24 +1,42 @@
 var snake;
+var snakeLength;
+var snakeSize;
 
-var content;
+var context;
 var screenWidth;
 var screenHeight;
 
+gameInitialize();
+gameDraw();
+
 function gameInitialize() {
     var canvas = document.getElementById("game-screen");
-    context = canvas.getContext("2D");
+    context = canvas.getContext("2d");
     
     screenWidth = window.innerWidth;
     screenHeight = window.innerHeight;
     
     canvas.width = screenWidth;
-    canvas.height = screenHeight;
-    
+    canvas.height = screenHeight; 
 }
+
 function gameLoop() {
     
 }
+
 function gameDraw() {
     context.fillStyle = "rgb(180, 250, 213)";
     context.fillRect(0, 0, screenWidth, screenHeight);
+}
+
+function snakeInitialize() {
+    
+}
+
+function snakeDraw() {
+    
+}
+
+function snakeUpdate() {
+    
 }
