8d7a6f4cf66f09a4031f87f04ad2bac4a82aeef6
Adrian Esquivel <Adrianesquivel1112@yahoo.com>
2014-10-09 11:24:36 -0700

    Snake
    making the snakes length, and size

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -30,7 +30,13 @@ function gameDraw() {
 }
 
 function snakeInitialize() {
+    snake = [];
+    snakeLength = 15;
+    snakeSize = 20;
     
+    for(var index = 0; index < snakeLength; index++) {
+        
+    }      
 }
 
 function snakeDraw() {
