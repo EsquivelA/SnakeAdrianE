c0c761b3583cebddb725a641f43cc700fc4280af
Adrian Esquivel <Adrianesquivel1112@yahoo.com>
2015-04-27 12:10:59 -0700

    Load
    now the player can register and load

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -17,6 +17,8 @@ var screenHeight;
 var gameState;
 var gameOverMenu;
 var restartButton;
+var playHUD;
+var scoreboard;
 
 /*-------------------------------------------------------------------------------------
  * Executing Game Code
@@ -49,13 +51,17 @@ function gameInitialize() {
     centerMenuPosition(gameOverMenu);
     
     restartButton = document.getElementById("restartButton");
-    restartButton.addEventListener("click", gameRestart)
+    restartButton.addEventListener("click", gameRestart);
+    
+    playHUD = document.getElementById("playHUD");
+    scoreboard = document.getElementById("scoreboard");
 
     setState("PLAY");
 }
 
 function gameLoop() {
     gameDraw();
+    drawScoreboard();
     if (gameState == "PLAY") {
         snakeUpdate();
         snakeDraw();
@@ -216,13 +222,24 @@ function displayMenu(menu) {
     menu.style.visibility = "visible";
 }
 
+function hideMenu(menu) {
+   menu.style.visibility = "hidden";
+}
+
 function showMenu(state) {
     if(state == "GAME OVER") {
         displayMenu(gameOverMenu);
+    }
+    else if(state == "PLAY") {
+        displayMenu(playHUD);
     }
 }
 
 function centerMenuPosition(menu) {
     menu.style.top = (screenHeight / 2) - (menu.offsetHeight / 2) + "px";
     menu.style.left = (screenWidth / 2) - (menu.offsetWidth / 2) + "px";
+}
+
+function drawScoreboard() {
+    scoreboard.innerHTML = "Length: " + snakeLength;
 }
